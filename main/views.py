26346e066f348a6d9bd389c6f27c7a5d42686c9b
whdev36 <whdev36@gmail.com>
2025-04-07 19:06:57 +0500

    user login and messages

--- main/views.py
+++ main/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
-# home
+# Home
 def home(request):
     return render(request, 'home.html', {})
+
+# Login user
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have successfully logged in.')
+            next_page = request.GET.get('next', 'home')
+            return redirect(next_page)
+        else:
+            messages.warning(request, 'Check your username or password!')
+    return render(request, 'main/login.html', {})
